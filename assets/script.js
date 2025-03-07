5dadeef0366dbeff39aea13bd7de91519a4d7bc1
dylanrexdev <dylanrexdev@gmail.com>
2025-03-07 00:35:54 -0500

    added hamburger menu

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
+function menu_toggle() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "flex") {
       x.style.display = "none";
     } else {
-      x.style.display = "block";
+      x.style.display = "flex";
     }
   }
